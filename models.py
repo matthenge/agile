d258c24acd6d99691a473ff4d7ad9192153f2907
kburudi <bkevery@gmail.com>
2019-01-21 15:18:55 +0300

    [finishes #163357535]Finished comment Module

--- models.py
+++ models.py
@@ -14,8 +14,6 @@ class User(object):
         self.password = password
         self.role = "user"
 
-    def add_default_role(self)
-
 class Moderator(User):
     """docstring for Moderator."""
     pass
@@ -26,17 +24,45 @@ class Admin(Moderator):
 
 class Comment:
     """docstring for Comment."""
-    def get_comment(comment):
-        comment = input("Add your comment here: ")
-        commented_at = datetime.datetime.utcnow()
-        new_comment = {
-        "user" :
+    def __init__(self):
+        pass
+
+    def generate_new_id(self):
+        latestComment = {}
+        if COMMENT:
+            latestComment = COMMENT[-1]
+            last_id = latestComment["id"]
+        else:
+            last_id = 0
+
+        return last_id + 1
+
+    def get_comment(self,a_comment, username):
+        """ fetch comment from a user"""
+        self.username = username
+        self.comment = a_comment
+        commentId = self.generate_new_id()
+        comment = a_comment
+        commentedAt = datetime.datetime.utcnow().strftime("%D %H:%M")
+        newComment = {
+        "id" : commentId,
+        "comment": a_comment,
+        "commented at" : commentedAt,
+        "user": username
         }
-    def add_comment()
+        return newComment
+
+    def add_comment(self):
+        comment = self.get_comment(self.username, self.comment)
+        COMMENT.append(comment)
+        print(comment)
+        print("Comment Has been added")
+        print(COMMENT)
+
 
 
 
 if __name__ == '__main__':
-    while True:
-        "Welcome to The agile group challenge: "
-        pass
+    myComment = Comment()
+    myComment.get_comment("Miss me","adnnn")
+    myComment.add_comment()
