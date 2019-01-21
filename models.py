be857cc51554b07ba4ff87ead9639a5dc3bffc15
kburudi <bkevery@gmail.com>
2019-01-21 16:05:53 +0300

    set up main running area[

--- models.py
+++ models.py
@@ -22,13 +22,12 @@ class User(object):
             "password": self.password,
             "role": self.role
         }
-
         USER.append(newUser)
-        
-     def edit_comment(self, com_id):
+
+    def edit_comment(self, com_id,username):
         """ Edits user comment """
         for user in LOGGED_IN:
-            if user['username'] == self.username: 
+            if user['username'] == username:
                 for com in COMMENT:
                     if com['id'] == com_id:
                         comment = com
@@ -40,12 +39,14 @@ class User(object):
 
                         comment['commented at'] = datetime.datetime.utcnow().strftime("%D %H:%M")
                         print(comment)
-                        
+
                 else:
                     print('You are not allowed to edit this comment')
+                    break
 
             else:
                 print('Please login')
+                break
 
     def login_user(self, username, password):
         """Login user"""
@@ -58,8 +59,10 @@ class User(object):
                     "lastLoggedInAt": datetime.datetime.utcnow().strftime("%D %H:%M")
                 }
                 logger.append(logged_in)
+                print (logger)
             else:
                 print("username or password are incorrect")
+                break
         return username
 
     def logout_user(self, username):
@@ -70,22 +73,41 @@ class User(object):
             if user["username"] == username:
                 logger.remove(user)
                 print("Logged out!")
+                break
+
+        print("I dont even know how you logged in")
+
 
 class Moderator(User):
     """docstring for Moderator."""
     def delete_comment(self, com_id):
         """ delete user comment """
         for user in LOGGED_IN:
-            if user['username'] == self.username: 
+            if user['username'] == self.username:
                 for com in COMMENT:
-                    if com['id'] == com_id: 
+                    if com['id'] == com_id:
                         COMMENT.remove(com)
                         print('COMMENT DELETED')
 
 
 class Admin(Moderator):
     """docstring for Admin."""
-    pass
+    @staticmethod
+    def delete_comment(comment_id):
+        for user in USER:
+            if user["role"] == "admin":
+                comm = {}
+                for comment in COMMENT:
+                    if comment.comment_id == comment_id:
+                        comm = comment
+                        COMMENT.remove(comment)
+                        print("comment has been deleted")
+                        print(COMMENT)
+                if not comm:
+                    print("Comment not found")
+                    return 0
+                print("You haveto be an admin to perform this action")
+                return 1
 
 class Comment:
     """docstring for Comment."""
@@ -107,6 +129,7 @@ class Comment:
         self.username = username
         self.comment = a_comment
         commentId = self.generate_new_id()
+        self.cid = commentId
         comment = a_comment
         commentedAt = datetime.datetime.utcnow().strftime("%D %H:%M")
         newComment = {
@@ -123,12 +146,19 @@ class Comment:
         print(comment)
         print("Comment Has been added")
         print(COMMENT)
+        return self.cid
 
 
 
 
 if __name__ == '__main__':
+    iAm = User("myUser", "password","Newser")
+    iAm.signup_user()
+    logged_user = iAm.login_user("myUser","password")
+    print(logged_user)
     myComment = Comment()
-    myComment.get_comment("Miss me","adnnn")
-    myComment.add_comment()
-
+    myComment.get_comment(logged_user, "Mi`ss me")
+    the_id = myComment.add_comment()
+    iAm.login_user(logged_user,"password")
+    iAm.edit_comment(the_id, logged_user)
+    Admin.delete_comment(the_id)
